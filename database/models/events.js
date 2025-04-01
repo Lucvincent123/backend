1e006dd80ca63ba578fcf81af582105d9724943b
Paul Roche <p.roche913@gmail.com>
2025-04-01 15:24:06 +0200

    Update events.js

--- database/models/events.js
+++ database/models/events.js
@@ -1,1 +1,26 @@
+const mongoose = require("mongoose");
 
+const eventSchema = new mongoose.Schema({
+    ID: {
+        type: Int,
+        required: true
+    },
+    Événement: {
+        type: String,
+        required: true
+    },
+    Année: {
+        type: Int,
+        required: true
+    },
+    Catégorie: {
+        type: Array,
+        required: true
+    },
+    Image: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("Event", eventSchema);
