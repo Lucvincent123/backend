5d24e11dce4f9147b80fa352a1a9a2eab26cccd3
Paul Roche <p.roche913@gmail.com>
2025-04-02 11:17:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
-require("dotenv").config()
-
+require("dotenv").config();
 const express = require('express');
-const cors = require("cors")
+const cors = require("cors");
 const connectDB = require("./database/connect");
 
-const userRouter = require("./routes/user.route")
-
-
+const userRouter = require("./routes/user.route");
+const eventRouter = require("./routes/event.route"); // Ajouter cette ligne pour les événements
 
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-// Define a route
+// Define routes
 app.get('/api', (req, res) => {
     res.send('Welcome to the Express.js Tutorial');
 });
 
-app.use("/api/users", userRouter)
-
+app.use("/api/users", userRouter);
+app.use("/api/events", eventRouter); // Ajouter cette route
 
 // Start the server
 app.listen(5000, () => {
