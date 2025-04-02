74c36994c11bbcbc8b102f0299a1eb0f1d990913
Paul Roche <p.roche913@gmail.com>
2025-04-02 11:17:53 +0200

    Update event.route.js

--- routes/event.route.js
+++ routes/event.route.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const Event = require('../models/Event'); // Import du modèle Event
+const event = require('../database/models/event'); // Import du modèle Event
 const router = express.Router();
 
 // Route pour récupérer tous les événements
 router.get('/', async (req, res) => {
     try {
-        const events = await Event.find(); // Récupérer tous les événements dans la base de données
+        const events = await event.find(); // Récupérer tous les événements dans la base de données
         res.json(events); // Retourner les événements au format JSON
     } catch (error) {
         console.error('Erreur lors de la récupération des événements:', error);
